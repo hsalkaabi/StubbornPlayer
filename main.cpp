e5d64139975a65ff34dd6265969e642a2708359f
hsalkaabi <hsalkaabi@mail.csuchico.edu>
2018-03-01 19:04:24 -0800

    SecretDoor

--- main.cpp
+++ main.cpp
@@ -6,6 +6,28 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int repeat;
+  int winner;
+
+    cout<<" How many times do you want to play?\n";
+    cin>> repeat;
+
+    for( int index=0; index<repeat; index++)
+    {
+
+            game.newGame();
+            game.guessDoorC();
+            game.guessDoorC();
+    if(game.isWinner() == true)
+    {
+            winner++;
+
+    }
+
+    }
+
+    cout<<"How many times you won?\n"<<winner<<endl;
+
+
   return 0;
 }
